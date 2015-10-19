e3a08caf2d08596ab49f0691d8463df3ef7e8b6a
Jacob Burton <session1@START-Coding-MBP-03.local>
2015-10-19 14:37:59 -0700

    Version 2 with fixed removal.

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function submitForm() {
     // Find a <table> element with id="results":
     var table = document.getElementById("results");
 
-    // Find a form with id="foodtracker"
-    var form = document.getElementById("foodtracker");
-
     // Call Input Variables
     var foodname = document.getElementsByName("foodname")[0].value;
     var purchaseDate = document.getElementsByName("purchase")[0].value;
     var expiration = document.getElementsByName("expiration")[0].value;
     var description = document.getElementsByName("description")[0].value;
+    var clearButton = "<button type='button' onclick='clearRow(this)'>Remove</button>";
+
+    // Find a form with id="foodtracker":
+    var form = document.getElementById("foodtracker");
 
     // Creates an empty row:
     var row = table.insertRow(1);
@@ -19,13 +20,20 @@ function submitForm() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
 
     // Add the text into the cells:
     cell1.innerHTML = foodname;
     cell2.innerHTML = purchaseDate;
     cell3.innerHTML = expiration;
     cell4.innerHTML = description;
+    cell5.innerHTML = clearButton;
 
     // Clear User Input in form:
     document.getElementById("foodtracker").reset();
+}
+
+function clearRow(r){
+    var findRow = r.parentNode.parentNode.rowIndex;
+    document.getElementById("results").deleteRow(findRow);
 }
